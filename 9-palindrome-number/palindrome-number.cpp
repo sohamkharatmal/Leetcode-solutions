c6ca35a31687082dd8a005acf294e7c6bc3631c6
SOHAM KHARATMAL <250192811+sohamkharatmal@users.noreply.github.com>
2026-04-02 19:39:18 +0530

    Time: 10 ms (6.57%) | Memory: 12.2 MB (7.82%) - LeetSync

---
+++ 9-palindrome-number/palindrome-number.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    
+    bool isPalindrome(int x) {
+        if(x<0)return false;
+        else if(x==0) return true;
+
+        vector <int> nums;
+        while(x>=1){
+            int z=x%10;
+            x/=10;
+            nums.push_back(z);
+        }
+        int l=0;
+        int r=nums.size()-1;
+        while(l<r){
+            if(nums[l]!=nums[r]) return false;
+            l++;
+            r--;
+        }
+        return true;
+    }
+};
