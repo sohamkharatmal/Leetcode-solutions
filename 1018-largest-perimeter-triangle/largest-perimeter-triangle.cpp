21b33ca6d5322233d2c29db91dc75be88b69cd8d
SOHAM KHARATMAL <sohamk8c2220@gmail.com>
2026-04-03 20:07:04 +0530

    Time: 0 ms (100.00%) | Memory: 25.7 MB (46.63%) - LeetSync

--- 1018-largest-perimeter-triangle/largest-perimeter-triangle.cpp
+++ 1018-largest-perimeter-triangle/largest-perimeter-triangle.cpp
@@ -3,7 +3,7 @@ public:
 
 
     int largestPerimeter(vector<int>& nums) {
-       if(nums.size()<3) return 0;
+     
         sort(nums.begin(),nums.end());
         for(int i=nums.size()-1;i>=2;i--){
             if(nums[i-1]+nums[i-2]>nums[i]) return nums[i]+nums[i-1]+nums[i-2];
